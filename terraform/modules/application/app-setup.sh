67717fd06a50f1d3e0845e78ff3544c9dcc23511
Akkamahadevi Hiremath <akkammah626@gmail.com>
2021-01-07 20:49:25 -0700

    update userdata to log userdata execution

--- terraform/modules/application/app-setup.sh
+++ terraform/modules/application/app-setup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 apt-get update
 apt-get install -y build-essential libssl-dev libffi-dev python-dev
