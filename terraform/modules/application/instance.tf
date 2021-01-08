d846c8812743971eb1ea67a4329641405b17a30f
Akkamahadevi Hiremath <akkammah626@gmail.com>
2021-01-07 21:45:03 -0700

    convert stand alone ec2 instance to asg - add lc and attach target group in asg

--- terraform/modules/application/instance.tf
+++ terraform/modules/application/instance.tf
@@ -13,33 +13,42 @@ data "aws_ami" "db_labs_ami" {
   owners = ["099720109477"] # Canonical
 }
 
-
-resource "aws_instance" "app_server" {
-  ami                         = data.aws_ami.db_labs_ami.image_id
-  instance_type               = "t3.micro"
-
+resource "aws_launch_configuration" "app_server_lc" {
+  name_prefix                   =  "app-server-lc-"
+  image_id                      =  data.aws_ami.db_labs_ami.image_id
+  instance_type                 = "t3.micro"
   key_name                      = "devi-us-west-1"
+  security_groups               = [aws_security_group.app_server_ssh.id]
+  user_data                     = file("${path.module}/app-setup.sh")
   associate_public_ip_address   = false
-  subnet_id                     = var.private_subnet_ids[0]
-
-  vpc_security_group_ids = [aws_security_group.app_server_ssh.id]
   
-  user_data = file("${path.module}/app-setup.sh")
+}
 
-  tags = {
-    Environment = var.environment
-    Name        = var.service_name
+resource "aws_autoscaling_group" "app_server_asg" {
+  name                 = "app-server-asg"
+  launch_configuration = aws_launch_configuration.app_server_lc.name
+  min_size             = 1
+  max_size             = 1
+  vpc_zone_identifier  = var.private_subnet_ids
+  tag {
+    key   = "Environment"
+    value = var.environment
+    propagate_at_launch = true
   }
-
+  tag {
+    key   = "Name"
+    value = var.service_name
+    propagate_at_launch = true
+  }
   lifecycle {
-    ignore_changes = [ ami ]
+    create_before_destroy = true
   }
 }
 
-resource "aws_lb_target_group_attachment" "app_tg_attachment" {
-  target_group_arn = aws_alb_target_group.app_alb_target_group.arn
-  target_id        = aws_instance.app_server.id
-  port             = 5000
+
+resource "aws_autoscaling_attachment" "app_tg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.app_server_asg.id
+  alb_target_group_arn = aws_alb_target_group.app_alb_target_group.arn
 }
 
 resource "aws_security_group" "app_server_ssh" {
