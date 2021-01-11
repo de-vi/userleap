4167b22dcba71ea5e72bcf9732a3c774f63d0e5f
Akkamahadevi Hiremath <akkammah626@gmail.com>
2021-01-11 13:39:17 -0700

    add infra and app test

---
+++ terraform/test/app_test.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAppInfra(t *testing.T) {
+	// Terraform options
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "..",
+	})
+
+	// Terraform Init & Apply
+	t.Run("Terraform Init & Apply", func(t *testing.T) {
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	// Some infra output validation
+	t.Run("Test Infra Output", func(t *testing.T) {
+		output := terraform.Output(t, terraformOptions, "app_endpoint")
+		if "app.userleap.de-vi.me" != output {
+			t.Errorf("Output not as expected. %s != \"app.userleap.de-vi.me\"", output)
+		}
+	})
+
+	// Test App Endpoint
+	t.Run("Test App Endpoint", func(t *testing.T) {
+		output := terraform.Output(t, terraformOptions, "app_endpoint")
+		resp, err := http.Get("https://" + output)
+		if err != nil {
+			t.Errorf("Error in get request to app: %s ", err)
+		}
+		if resp == nil {
+			t.Errorf("Got no response from app")
+		} else {
+			if resp.StatusCode != 200 {
+				t.Errorf("Invalid response code. %d != 200", resp.StatusCode)
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("Error in http response parsing: %s ", err)
+			}
+			if string(body) != "<html><h1>IT WORKS!</h1></html>" {
+				t.Errorf("Unexpected http response. \"%s\" != \"<html><h1>IT WORKS!</h1></html>\"", string(body))
+			}
+		}
+	})
+	// Terraform destroy after all tests
+	t.Run("Test Destroy", func(t *testing.T) {
+		terraform.Destroy(t, terraformOptions)
+	})
+}
---
+++ terraform/test/go.mod
@@ -0,0 +1,16 @@
+module test
+
+go 1.13
+
+require (
+	github.com/gruntwork-io/terratest v0.31.3
+	github.com/jinzhu/copier v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20210110051926-789bb1bd4061 // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
