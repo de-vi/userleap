37746470750dfce3143e44b0b2ce44486344a233
Akkamahadevi Hiremath <akkammah626@gmail.com>
2021-01-15 08:56:12 -0700

    update variable definitions

--- terraform/README.md
+++ terraform/README.md
@@ -2,6 +2,7 @@
 
 | Name | Version |
 |------|---------|
+| terraform | = 0.13.6 |
 | aws | = 3.23.0 |
 
 ## Providers
@@ -15,31 +16,30 @@
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| container\_name | n/a | `any` | n/a | yes |
-| container\_port | n/a | `any` | n/a | yes |
-| ecs\_image\_id | n/a | `any` | n/a | yes |
-| ecs\_image\_version | n/a | `any` | n/a | yes |
-| environment | The environment | `any` | n/a | yes |
-| key\_pair\_name | n/a | `any` | n/a | yes |
-| private\_subnets\_cidr | n/a | `list` | n/a | yes |
-| project\_env | n/a | `any` | n/a | yes |
-| public\_subnets\_cidr | n/a | `list` | n/a | yes |
-| region | n/a | `any` | n/a | yes |
-| route53\_domain\_name | n/a | `any` | n/a | yes |
-| route53\_record\_name | n/a | `any` | n/a | yes |
-| service\_name | n/a | `any` | n/a | yes |
-| vpc\_cidr | n/a | `any` | n/a | yes |
-| alias\_records | n/a | `list` | `[]` | no |
-| egress\_instance\_cidr\_rules | n/a | `list` | `[]` | no |
-| egress\_instance\_sg\_rules | n/a | `list` | `[]` | no |
-| egress\_lb\_sg\_rules | n/a | `list` | `[]` | no |
-| ingress\_instance\_sg\_rules | n/a | `list` | `[]` | no |
-| ingress\_lb\_cidr\_rules | n/a | `list` | `[]` | no |
-| ingress\_lb\_sg\_rules | n/a | `list` | `[]` | no |
-| key\_public\_material | n/a | `string` | `""` | no |
-| listeners | n/a | `list` | `[]` | no |
-| target\_group\_health\_checks | n/a | `list` | `[]` | no |
-| target\_groups | n/a | `list` | `[]` | no |
+| container\_name | Container name for task definiton | `string` | n/a | yes |
+| container\_port | Container port where application is running | `string` | n/a | yes |
+| ecs\_image\_id | ECR image name | `string` | n/a | yes |
+| ecs\_image\_version | ECR image version | `string` | n/a | yes |
+| environment | Name of your environment | `string` | n/a | yes |
+| key\_pair\_name | Name of the keypair available in the region where the application is deployed, mutually exclusive with key\_public\_material | `string` | n/a | yes |
+| private\_subnets\_cidr | CIDR for private subnets | `list` | n/a | yes |
+| project\_env | system environment, for example dev, test or uat | `string` | n/a | yes |
+| public\_subnets\_cidr | CIDR for public subnets | `list` | n/a | yes |
+| region | Region in which you want to create infrastructure and deploy application | `string` | n/a | yes |
+| route53\_domain\_name | Name of the Route53 domain in which application will be hosted | `string` | n/a | yes |
+| route53\_record\_name | DNS name for application, must be a subdomain of route53\_domain\_name | `string` | n/a | yes |
+| service\_name | Name of your service | `string` | n/a | yes |
+| vpc\_cidr | CIDR for VPC | `string` | n/a | yes |
+| egress\_instance\_cidr\_rules | List of outbound security group rules for instance with source security CIDR blocks | `list(map(string))` | `[]` | no |
+| egress\_instance\_sg\_rules | List of outbound security group rules for instance with source security group ids | `list(map(string))` | `[]` | no |
+| egress\_lb\_sg\_rules | List of outbound security group rules for ALB with source security group ids | `list(map(string))` | `[]` | no |
+| ingress\_instance\_sg\_rules | List of inbound security group rules for instance with source security group ids | `list(map(string))` | `[]` | no |
+| ingress\_lb\_cidr\_rules | List of inbound security group rules for ALB with source security CID blocks | `list(map(string))` | `[]` | no |
+| ingress\_lb\_sg\_rules | List of inbound security group rules for ALB with source security group ids | `list(map(string))` | `[]` | no |
+| key\_public\_material | Publi key material using which a new keypair can be created, mutually exclusive with key\_pair\_name | `string` | `""` | no |
+| listeners | List of listeners for ALB | `list(map(string))` | `[]` | no |
+| target\_group\_health\_checks | List of health checks for each target group, must contain same number of elements as target\_groups | `list(map(string))` | `[]` | no |
+| target\_groups | List of target groups to create | `list(map(string))` | `[]` | no |
 
 ## Outputs
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,60 +1,129 @@
-variable "vpc_cidr" {}
+variable "region" {
+  description = "Region in which you want to create infrastructure and deploy application"
+  type        = string
+}
 
-variable "region" {}
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type        = string
+}
 
 variable "public_subnets_cidr" {
-  type = list
+  description = "CIDR for public subnets"
+  type        = list
 }
 
 variable "private_subnets_cidr" {
-  type = list
+  description = "CIDR for private subnets"
+  type        = list
 }
 
 variable "environment" {
-  description = "The environment"
+  description = "Name of your environment"
+  type        = string
 }
 
-variable "service_name" {}
-variable "project_env" {}
-variable "key_pair_name" {}
+variable "service_name" {
+  description = "Name of your service"
+  type        = string
+}
+
+variable "project_env" {
+  description = "system environment, for example dev, test or uat"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Name of the keypair available in the region where the application is deployed, mutually exclusive with key_public_material"
+  type        = string
+}
+
 variable "key_public_material" {
-  default = ""
+  description = "Publi key material using which a new keypair can be created, mutually exclusive with key_pair_name"
+  type        = string
+  default     = ""
 }
-variable "alias_records" {
-  default = []
+
+variable "route53_domain_name" {
+  description = "Name of the Route53 domain in which application will be hosted"
+  type        = string
 }
-variable "route53_domain_name" {}
-variable "route53_record_name" {}
+
+variable "route53_record_name" {
+  description = "DNS name for application, must be a subdomain of route53_domain_name"
+  type        = string
+}
 
 variable "target_groups" {
-  default = []
-}
-variable "target_group_health_checks" {
-  default = []
-}
-variable "listeners" {
-  default = []
-}
-variable "ingress_instance_sg_rules" {
-  default = []
-}
-variable "egress_instance_sg_rules" {
-  default = []
-}
-variable "ingress_lb_sg_rules" {
-  default = []
-}
-variable "egress_lb_sg_rules" {
-  default = []
-}
-variable "ingress_lb_cidr_rules" {
-  default = []
-}
-variable "egress_instance_cidr_rules" {
-  default = []
+  description = "List of target groups to create"
+  type        = list(map(string))
+  default     = []
 }
 
-variable "ecs_image_id" {}
-variable "ecs_image_version" {}
-variable "container_name" {}
-variable "container_port" {}
+variable "target_group_health_checks" {
+  description = "List of health checks for each target group, must contain same number of elements as target_groups"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "listeners" {
+  description = "List of listeners for ALB"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_instance_sg_rules" {
+  description = "List of inbound security group rules for instance with source security group ids"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_instance_sg_rules" {
+  description = "List of outbound security group rules for instance with source security group ids"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_lb_sg_rules" {
+  description = "List of inbound security group rules for ALB with source security group ids"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_lb_sg_rules" {
+  description = "List of outbound security group rules for ALB with source security group ids"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_lb_cidr_rules" {
+  description = "List of inbound security group rules for ALB with source security CID blocks"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_instance_cidr_rules" {
+  description = "List of outbound security group rules for instance with source security CIDR blocks"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ecs_image_id" {
+  description = "ECR image name"
+  type        = string
+}
+
+variable "ecs_image_version" {
+  description = "ECR image version"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Container name for task definiton"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Container port where application is running"
+  type        = string
+}
